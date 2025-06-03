e5bc7dfb41dc280a2af16b5b3bad25edfc04cf37
Anurag Adhikari <anuragadhikari5276@gmail.com>
2025-06-03 22:45:15 +0545

    define custom type

---
+++ 02-customType.ts
@@ -0,0 +1,20 @@
+type Person = {
+  name: string;
+  age: number;
+  isStudent: boolean;
+  hobbies: string[];
+};
+
+let person1: Person = {
+  name: "Alice",
+  age: 21,
+  isStudent: true,
+  hobbies: ["reading", "hiking", "coding"],
+};
+
+let person2: Person = {
+  name: "Bob",
+  age: 30,
+  isStudent: false,
+  hobbies: ["gaming", "cooking"],
+};
