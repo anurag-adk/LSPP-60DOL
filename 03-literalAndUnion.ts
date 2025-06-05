1cec7ad54fd9a7183d9ccadf359f43beeceec5dd
Anurag Adhikari <anuragadhikari5276@gmail.com>
2025-06-05 22:29:31 +0545

    export userRole from 03 and import in 04

--- 03-literalAndUnion.ts
+++ 03-literalAndUnion.ts
@@ -1,4 +1,4 @@
-type userRole = "admin" | "buyer" | "seller";
+export type userRole = "admin" | "buyer" | "seller"; //exporting to import in other files
 //This is called union type, it is like giving limited options to a variable type.
 
 let user: userRole = "seller";
---
+++ 04-returnType-UtilityType-Partials.ts
@@ -0,0 +1,27 @@
+import type { userRole } from "./03-literalAndUnion.ts";
+
+type User = {
+  id: number;
+  username: string;
+  role: userRole; //from 03-literalAndUnion.ts
+};
+
+const users: User[] = [
+  { id: 1, username: "harry", role: "admin" },
+  { id: 2, username: "ron", role: "buyer" },
+  { id: 3, username: "hermione", role: "seller" },
+];
+
+//FUNCTION RETURN TYPE
+//Syntax is, function_name(argument: type): returnType
+function getUserDetails(username: string): User | undefined {
+  const user = users.find((user) => user.username === username);
+  if (!user) {
+    console.error(`User ${username} not found`);
+    return;
+  }
+  return user;
+}
+
+function updateUser(id: number, changes: any) {}
+console.log(getUserDetails("hermione"));
